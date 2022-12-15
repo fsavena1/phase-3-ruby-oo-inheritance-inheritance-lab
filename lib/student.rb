02961a45d261533a0f097a4fdfea6221f31cc051
frank <franksavena@gmail.com>
2022-12-15 08:41:31 -0600

    commit tree fd851a2a9822ee3cdbcabb595979f234cc6835c7

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,23 @@
-class Student 
+require_relative './user.rb'
+require 'pry'
+
+class Student < User
+
+    def initialize(knowledge = [])
+        @knowledge = knowledge
+    end
+
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
+
+    def knowledge
+        @knowledge
+    end
+
 
 end
+
+# student = Student.new("test", "test")
+
+# binding.pry
